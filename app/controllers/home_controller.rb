9d2b31f34fe56684a7161f5c3c16bfe9201055ab
L Karthik <l.karthik@ericsson.com>
2016-12-26 18:06:51 +0530

    home page folder structure display and properties

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    all_file_infos = FileInfo.includes(:component).all
+    @result_hash = { '/' => { files: [], folders: {} } }
+    all_file_infos.each do |fi|
+      add_path_to_hash(@result_hash['/'], fi)
+    end
   end
+
+  private
+    def add_path_to_hash(ptr, fi)
+      path_elements = fi.folder.split('/').reject(&:blank?)
+      path_elements.each do |element|
+        ptr = ptr[:folders]
+        ptr[element] ||= { files: [], folders: {}}
+        ptr = ptr[element]
+      end
+      ptr[:files] << fi
+    end
 end

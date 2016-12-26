e2db49cdc4df6c0fd268d4b13e2c43f954038694
L Karthik <l.karthik@ericsson.com>
2016-12-26 12:17:31 +0530

    home controller and dummy index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :components
   resources :file_infos
   # The priority is based upon order of creation: first created -> highest priority.

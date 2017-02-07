5d7873ddc93aca965492f700ef1c00c962bc2f7c
Karthik L <L.Karthik@Ericsson.com>
2017-02-07 16:55:48 +0530

    cleanup of home controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/pretty_view'
-  get 'home/form_view'
-  get 'home/treeview_data_json'
-  get 'home/test_draggable'
 
   resources :components
   resources :file_infos

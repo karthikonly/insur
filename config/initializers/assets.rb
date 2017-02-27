c3407196b29d0478affc9ca4ffeb76c08f162849
Karthik L <karthikonly@gmail.com>
2017-02-27 21:48:39 +0530

    cleanup assets directory

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( home_page.js pretty_view.js )
+Rails.application.config.assets.precompile += %w( drag_and_drop.js )

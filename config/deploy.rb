f69f2ca891c5390891db5624d5bf5a581f973feb
L Karthik <l.karthik@ericsson.com>
2017-03-01 15:13:56 +0530

    capistrano production bug fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,13 +19,13 @@ set :rvm_ruby_version, 'ruby-2.2.4'
 
 # use append command to add to existing variable. default is [], so we can use set
 # set :linked_files, %w{config/mongoid.yml config/secrets.yml config/application.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/assets}
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-# set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
